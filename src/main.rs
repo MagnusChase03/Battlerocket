72feaa261eb9a1bd23c085aea08203d3fbdb8a14
Zachary <zachness2003@gmail.com>
2022-09-29 20:07:21 -0500

    Can add two node rocket

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,53 @@ fn make_board() -> [[[i32; 10]; 10]; 10] {
 }
 
 // Places a rocket with only one node
-fn place_simple_rocket(game_board: &mut [[[i32; 10]; 10]; 10]) {
+fn place_node_rocket(game_board: &mut [[[i32; 10]; 10]; 10]) {
 
-    println!("Enter x y z: ");
+    println!("Enter cords for single node rocket (x y z): ");
 
     let mut position: String = String::new();
     std::io::stdin().read_line(&mut position).unwrap();
+    position.pop();
+    position.pop();
+
+    let cords = position.split(" ").collect::<Vec<&str>>();
+    println!("{:?}",cords);
+    let x = cords[0].parse::<usize>().unwrap();
+    let y = cords[1].parse::<usize>().unwrap();
+    let z = cords[2].parse::<usize>().unwrap();
+
+    game_board[x][y][z] = 1;
+
+}
+
+// Place two node rocket
+fn place_rocket(game_board: &mut [[[i32; 10]; 10]; 10]) {
+
+    println!("Enter cords for two node rocket(x+1) (x y z): ");
+
+    let mut position: String = String::new();
+    std::io::stdin().read_line(&mut position).unwrap();
+    position.pop();
+    position.pop();
 
     let cords = position.split(" ").collect::<Vec<&str>>();
     let x = cords[0].parse::<usize>().unwrap();
-    let y = cords[0].parse::<usize>().unwrap();
-    let z = cords[0].parse::<usize>().unwrap();
+    let y = cords[1].parse::<usize>().unwrap();
+    let z = cords[2].parse::<usize>().unwrap();
+
+    let rocket = rockets::Rocket {x: x, y: y, z: z};
+    let second_node = rocket.next();
 
     game_board[x][y][z] = 1;
+    game_board[second_node.0][second_node.1][second_node.2] = 1;
 
 }
 
 // Place rockets on board
 fn place_rockets(game_board: &mut [[[i32; 10]; 10]; 10]) {
     
-    place_simple_rocket(game_board);
+    place_node_rocket(game_board);
+    place_rocket(game_board);
 
 }
 
--- src/rockets.rs
+++ src/rockets.rs
@@ -1,6 +1,17 @@
-pub struct rocket {
+pub struct Rocket {
 
-    pub x: i32,
-    pub y: i32
+    pub x: usize,
+    pub y: usize,
+    pub z: usize
+
+}
+
+impl Rocket {
+
+    pub fn next(&self) -> (usize, usize, usize) {
+
+        (self.x + 1, self.y, self.z)
+
+    }
 
 }
