28c8eaa03e11fe1a0c41850df954a084cee2442d
Zachary <zachness2003@gmail.com>
2022-10-02 11:23:47 -0500

    Base game done... I think

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ fn make_board() -> [[[i32; 10]; 10]; 10] {
     board
 }
 
+// TODO Make function to just create a cordinate from system input for code reuse
+
 // Get the origin the ship should be placed at
 fn get_orgin(rocket: &mut rockets::Rocket) {
-
-    println!("Enter cords for single node rocket (x y z): ");
+    println!("Enter origin for {} node rocket (x y z): ", rocket.size);
 
     let mut position: String = String::new();
     std::io::stdin().read_line(&mut position).unwrap();
@@ -25,13 +26,11 @@ fn get_orgin(rocket: &mut rockets::Rocket) {
     let z = cords[2].parse::<usize>().unwrap();
 
     rocket.origin = (x, y, z);
-
 }
 
 // Gets the direction the ship should be placed in
 fn get_direction(rocket: &mut rockets::Rocket) {
-
-    println!("Enter direction for rocket (x y z): ");
+    println!("Enter direction for {} node rocket (x y z): ", rocket.size);
 
     let mut position: String = String::new();
     std::io::stdin().read_line(&mut position).unwrap();
@@ -46,53 +45,113 @@ fn get_direction(rocket: &mut rockets::Rocket) {
     let z = cords[2].parse::<usize>().unwrap();
 
     rocket.direction = Some((x, y, z));
-
 }
 
 // Places rocket on game board
 fn place_rocket(game_board: &mut [[[i32; 10]; 10]; 10], rocket: &mut rockets::Rocket) {
-
     rocket.calculate_nodes();
 
     for node in &rocket.nodes {
-
-        game_board[node.0][node.1][node.2] = 1;
-
+        game_board[node.2][node.1][node.0] = 1;
     }
-
 }
 
 // Create and place rocket
-fn create_rocket(game_board: &mut [[[i32; 10]; 10]; 10], size: i32) {
-
+fn create_rocket(game_board: &mut [[[i32; 10]; 10]; 10], size: i32) -> rockets::Rocket {
     let mut rocket = rockets::Rocket::new(0, 0, 0, size);
     get_orgin(&mut rocket);
 
     if size > 1 {
-
         get_direction(&mut rocket);
-
     }
-    
+
     place_rocket(game_board, &mut rocket);
 
+    rocket
 }
 
 // Place rockets on board
-fn place_rockets(game_board: &mut [[[i32; 10]; 10]; 10]) {
-    
-    create_rocket(game_board, 1);
-    create_rocket(game_board, 2);
-    create_rocket(game_board, 3);
+fn place_rockets(game_board: &mut [[[i32; 10]; 10]; 10]) -> Vec<rockets::Rocket> {
+    let mut placed_rockets: Vec<rockets::Rocket> = Vec::new();
+
+    placed_rockets.push(create_rocket(game_board, 1));
+    placed_rockets.push(create_rocket(game_board, 2));
+    placed_rockets.push(create_rocket(game_board, 3));
+    placed_rockets.push(create_rocket(game_board, 4));
+    placed_rockets.push(create_rocket(game_board, 5));
+
+    placed_rockets
+}
+
+// Check if all player rockets are destroyed
+fn player_dead(player_rockets: &Vec<rockets::Rocket>, game_board: &[[[i32; 10]; 10]; 10]) -> bool {
+    for rocket in player_rockets {
+        if !rocket.is_dead(game_board) {
+            return false;
+        }
+    }
+
+    true
+}
+
+// Let player attack spot on enemy board
+fn player_attack(game_board: &mut [[[i32; 10]; 10]; 10]) -> bool {
+
+    println!("Enter cords to attack (x y z): ");
+
+    let mut position: String = String::new();
+    std::io::stdin().read_line(&mut position).unwrap();
+
+    // Remove new line and return carage
+    position.pop();
+    position.pop();
+
+    let cords = position.split(" ").collect::<Vec<&str>>();
+    let x = cords[0].parse::<usize>().unwrap();
+    let y = cords[1].parse::<usize>().unwrap();
+    let z = cords[2].parse::<usize>().unwrap();
+
+    if game_board[z][y][x] == 1 {
+
+        game_board[z][y][x] = 0;
+        return true;
+
+    }
+
+    false
 
 }
 
 fn main() {
     let mut game_board1 = make_board();
-    //let mut game_board2 = make_board();
+    let mut game_board2 = make_board();
 
-    place_rockets(&mut game_board1);
-    //place_rockets(&mut game_board2);
+    println!("Player 1: Place rockets: ");
+    let player1_rockets = place_rockets(&mut game_board1);
 
-    print!("{:?}", game_board1);
+    println!("===================================");
+    print!("Player 2: Place rockets: ");
+    let player2_rockets = place_rockets(&mut game_board2);
+
+    let mut player_turn = 1;
+    while !player_dead(&player1_rockets, &game_board1)
+    && !player_dead(&player2_rockets, &game_board2)
+    {
+        
+        println!("===================================");
+        print!("Player {}: ", player_turn);
+        println!("{:?}", game_board1);
+        if player_turn == 1 {
+
+            player_attack(&mut game_board2);
+            player_turn = 2;
+
+        } else {
+
+            player_attack(&mut game_board1);
+            player_turn = 1;
+
+        }
+
+    }
 }
--- src/rockets.rs
+++ src/rockets.rs
@@ -16,24 +16,35 @@ impl Rocket {
     }
 
     pub fn calculate_nodes(&mut self) {
-
         self.nodes.push(self.origin);
         if self.size > 1 {
-
             let mut previous_node = self.origin;
             for _i in 0..(self.size - 1) {
                 let next_node = (
                     previous_node.0 + self.direction.unwrap().0,
                     previous_node.1 + self.direction.unwrap().1,
                     previous_node.2 + self.direction.unwrap().2,
-    
                 );
-    
+
                 self.nodes.push(next_node);
                 previous_node = next_node;
             }
+        }
+    }
+
+    pub fn is_dead(&self, game_board: &[[[i32; 10]; 10]; 10]) -> bool {
+
+        for node in &self.nodes {
+
+            if game_board[node.2][node.1][node.0] == 1 {
+
+                return false;
+
+            }
 
         }
+
+        true
 
     }
 }
